311e5b16f1fb8783a09378bde94c630c7f559280
Luis Vegas <luisvegasmor@gmail.com>
2021-07-19 23:51:14 -0400

    Wrapped the prompt logic in a single struct for the sake of modularity

---
+++ src/conway.rs
@@ -0,0 +1,32 @@
+use super::*;
+
+pub struct Conway {}
+impl Conway {
+    pub fn prompt(&self) {
+        print!("Conway> ");
+        io::stdout().flush().unwrap();
+    }
+
+    pub fn read_input(&self) {
+        let mut input_buffer = String::new();
+
+        io::stdin()
+            .read_line(&mut input_buffer)
+            .expect("Failed to read stdin");
+
+        let astnode = parse(&&input_buffer).expect("unsuccessful parse");
+        let mut int = Interpreter { env: Environment::new() };
+        for node in astnode.into_iter() {
+            println!("{}", int.eval(&node));
+        }
+    }
+
+    pub fn run(&self) {
+        loop {
+            self.prompt();
+            self.read_input();
+        }
+    }
+}
+
+
